4ed6c2fc799a6de0de58e0fcc41063607858260b
Felipe-Belotto <felipeeduardo817@gmail.com>
2024-03-12 18:50:49 -0300

    adicionando titulo na aba 404

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,5 +1,9 @@
 import PaginaNaoEncontrada from "@/Components/PaginaNaoEncontrada/PaginaNaoEncontrada";
 
+export const metadata = {
+  title: "404 | Portifolio",
+  description: "Página inicial do portifólio",
+};
 
 export default function NotFound() {
   
